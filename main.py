a0c637ad4483d162ceb9c6f5d11d6dc51d27a271
yohann.schatt <yohann.schatt@igtp3-pc14.isim.intra>
2023-11-21 13:32:04 +0100

    test du défilement du text sur l'écran

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import LCD
 import buzzer
 
 LCD.setColor("jaune")
-LCD.setText1("salut a tous les amis c'est david la farge poké�mon et on se retrouve avec miss jiraichi COUCOU")
+LCD.Text("salut a tous les amis c'est david la farge poké�mon et on se retrouve avec miss jiraichi COUCOU")
 
