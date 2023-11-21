486db25b0795e93449be3eb67545c59b2bbaee61
yohann.schatt <yohann.schatt@igtp3-pc14.isim.intra>
2023-11-21 12:44:22 +0100

    ajout des drivers du buzzer et de l'écran LCD (pas fini)

---
+++ LCD.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+import grovepi
+import smbus
+import time
+
+bus = smbus.SMBus(1)  # pour I2C-1 (0 pour I2C-0)
+
+# Indiquez ici les deux adresses de l'ecran LCD
+# celle pour les couleurs du fond d'ecran 
+# et celle pour afficher des caracteres
+DISPLAY_RGB_ADDR = 0x62
+DISPLAY_TEXT_ADDR = 0x3e
+
+# Completez le code de la fonction permettant de choisir la couleur
+# du fond d'ecran, n'oubliez pas d'initialiser l'ecran
+def setRGB(rouge,vert,bleu):
+        bus.write_byte_data(DISPLAY_RGB_ADDR,0x00,0x00)
+        bus.write_byte_data(DISPLAY_RGB_ADDR,0x01,0x00)
+        bus.write_byte_data(DISPLAY_RGB_ADDR,0x02,bleu)
+        bus.write_byte_data(DISPLAY_RGB_ADDR,0x03,vert)
+        bus.write_byte_data(DISPLAY_RGB_ADDR,0x04,rouge)
+        bus.write_byte_data(DISPLAY_RGB_ADDR,0x08,0xAA)
+        print("Couleur écran changée")
+
+# Envoie  a l'ecran une commande concerant l'affichage des caracteres
+# (cette fonction vous est donnes gratuitement si vous
+# l'utilisez dans la fonction suivante, sinon donnez 2000€
+# a la banque et allez dictement en prison :)
+def textCmd(cmd):
+        bus.write_byte_data(DISPLAY_TEXT_ADDR,0x80,cmd)
+        time.sleep(0.01)
+
+# Completez le code de la fonction permettant d'ecrire le texte recu en parametre
+# Si le texte contient un \n ou plus de 16 caracteres pensez a gerer
+# le retour a la ligne
+def Text(texte):
+        textCmd(0x01)
+        textCmd(0x0F)
+        textCmd(0x38)
+        compteur = 0
+        for c in texte :
+                bus.write_byte_data(DISPLAY_TEXT_ADDR,0x40,ord(c))
+                compteur += 1
+                if compteur == 16 or c == '\n':
+                        textCmd(0xc0)
+                        compteur = 0
+        print ("texte ecrit")
+
+def SetText1(texte):
+    nbrepet = len(texte)/32
+    i = 0
+    debi = 0
+    while(nbrepet == 0):
+        compteur = 0
+        for i in range(debi,debi+32):
+            bus.write_byte_data(DISPLAY_TEXT_ADDR,0x40,ord(c))
+            compteur += 1
+            if compteur == 16 or c == '\n':
+                    textCmd(0xc0)
+                    compteur = 0
+        time.sleep(2)
+        TextCmd(0x01)
+        TextCmd(0x0F)
+        TextCmd(0x38)
+        debi = debi + 32
+        nbrepet - 1
+
+
+def setColor(nomCouleur):
+    if nomCouleur == "rouge":
+        setRGB(255,0,0)
+    if nomCouleur == "bleu":
+        setRGB(0,0,255)
+    if nomCouleur == "vert":
+        setRGB(0,255,0)
+    if nomCouleur == "blanc":
+        setRGB(0,0,0)
+    if nomCouleur == "jaune":
+        setRGB(127,127,0)
+
+def setColor(nomCouleur):
+    if nomCouleur == "rouge":
+        setRGB(255,0,0)
+    if nomCouleur == "bleu":
+        setRGB(0,0,255)
+    if nomCouleur == "vert":
+        setRGB(0,255,0)
+    if nomCouleur == "blanc":
+        setRGB(0,0,0)
+    if nomCouleur == "jaune":
+        setRGB(127,127,0)
+
+
