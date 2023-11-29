fefa82e7f1503ee5aa01f25092bffe321ae11d10
yohann.schatt <yohann.schatt@igtp3-pc02.isim.intra>
2023-11-29 09:32:03 +0100

    voila

--- LCD2.py
+++ LCD2.py
@@ -66,12 +66,12 @@ def setText_norefresh(text):
         bus.write_byte_data(DISPLAY_TEXT_ADDR,0x40,ord(c))
 
 
-setText("Hello world\nThis is an LCD test")
-setRGB(0,128,64)
-time.sleep(2)
-for c in range(0,255):
-    setText_norefresh("Going to sleep in {}...".format(str(c)))
-    setRGB(c,255-c,0)
-    time.sleep(0.1)
-setRGB(0,255,0)
-setText("Bye bye, this should wrap onto next line")
+#setText("Hello world\nThis is an LCD test")
+#setRGB(0,128,64)
+#time.sleep(2)
+#for c in range(0,255):
+#    setText_norefresh("Going to sleep in {}...".format(str(c)))
+#    setRGB(c,255-c,0)
+#    time.sleep(0.1)
+#setRGB(0,255,0)
+#setText("Bye bye, this should wrap onto next line")
