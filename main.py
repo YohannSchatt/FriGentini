73c0b8420081ab9d94bbf77de85dd4c6b586e129
yohann.schatt <yohann.schatt@igtp3-pc02.isim.intra>
2023-11-29 09:22:44 +0100

    voila

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import LCD
+import LCD2
 import buzzer
 
 LCD.setColor("jaune")
-LCD.Text2("salut a tous les amis c'est david la farge poké�mon et on se retrouve avec miss jiraichi COUCOU")
+LCD2.setText("salut a tous les amis c'est david la farge poké�mon et on se retrouve avec miss jiraichi COUCOU")
 
