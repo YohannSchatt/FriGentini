2a2c55db9fa2b46ac3c8556c158ca4360b7ac3f2
mathias <mathias.puyfages@etu.umontpellier.fr>
2023-12-13 08:39:27 +0100

    Modification de la fonction ReadTeamperature

--- Python/DriverThermometre.py
+++ Python/DriverThermometre.py
@@ -8,4 +8,7 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 dps310 = DPS310(i2c)
 
 def ReadTemperature():
+    i2c = board.I2C()  # uses board.SCL and board.SDA
+    # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+    dps310 = DPS310(i2c)
     return dps310.temperature
