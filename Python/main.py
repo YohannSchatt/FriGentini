ca13517a2397485739c84c3dbd922db4a9e308dd
yohann.schatt <yohann.schatt@igtp3-pc07.isim.intra>
2023-12-15 14:46:59 +0100

    correction de bug

--- Python/main.py
+++ Python/main.py
@@ -4,8 +4,8 @@ from driverI2C import *
 import time
 
 Textinitialisation()
-setTextLigne1("Bonjour \n tu vois ce texte")
 setTextLigne2("je suis la")
+setTextLigne1("Bonjour \n tu vois ce texte")
 print("texte affiché")
 setRGB(0,128,64)
 
