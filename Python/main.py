6c3f822bf014bac6baecbf109c5085a29215d7d1
yohann.schatt <yohann.schatt@igtp3-pc07.isim.intra>
2023-12-15 17:16:29 +0100

    début du menu

--- Python/main.py
+++ Python/main.py
@@ -12,7 +12,7 @@ import grovepi
 
 buttonOk = 4
 buttonBack = 7
-buttonplus = 2
+buttonPlus = 2
 buttonMoins = 3
 
 LCD.effacerText()
@@ -24,21 +24,63 @@ time.sleep(2)
 #     température = thermo.ReadTemperature()
 #     print(température)
 #     LCD.setTextLigne1(str(round(température,2))+' Celsius')
-
+#     print(grovepi.digitalRead(buttonPlus))
 #Set up of the buttons : ports 7,4,3 and 2 (3 and 2 are tactils ones)
 # Connect the Grove Button to digital port D3
 # SIG,NC,VCC,GND
-button = 4
 
-grovepi.pinMode(button,"INPUT")
+grovepi.pinMode(buttonOk,"INPUT")
+grovepi.pinMode(buttonBack,"INPUT")
+grovepi.pinMode(buttonPlus,"INPUT")
+grovepi.pinMode(buttonMoins,"INPUT")
 
+def LectBouton():
+    if grovepi.digitalRead(buttonOk) == 1:
+        return "Ok"
+    if grovepi.digitalRead(buttonBack) == 1:
+        return "Back"
+    if grovepi.digitalRead(buttonPlus) == 1:
+        return "Plus"
+    if grovepi.digitalRead(buttonMoins) == 1:
+        return "Moins"
+pageMenu = 0
+selectionPage = 1
 while True:
-    try:
-        print(grovepi.digitalRead(buttonplus))
-        time.sleep(.5)
-
-    except IOError:
-        print ("Error")
+    Bouton = LectBouton()
+    if pageMenu == 0:
+        LCD.setTextLigne1("    Selection")
+        if selectionPage == 1:
+            LCD.setTextLigne2("< affiche Temp >")
+            if Bouton == "Ok":
+                pageMenu = 1
+        if selectionPage == 2:
+            LCD.setTextLigne2("<  ajout data  >")
+                if Bouton == "Ok":
+                    pageMenu = 2
+        if selectionPage == 3:
+            LCD.setTextLigne2("< afficher data >")
+            if Bouton == "Ok":
+                pageMenu = 3
+        if selectionPage == 4:
+            LCD.setTextLigne2("<  suppr data  >")
+            if Bouton == "Ok":
+                pageMenu = 4
+        if selectionPage == 5:
+            LCD.setTextLigne2("<   Eteindre   >")
+            if Bouton == "Ok":
+                pageMenu = 5
+        if Bouton == "Plus":
+            selectionPage = (selectionPage+1)%5
+        if Bouton == "Moins":
+            if selectionPage = 0:
+                selectionPage = 5
+            else :
+                selectionPage = selectionPage - 1
+    if pageMenu = 1 :
+        LCD.setTextLigne1(str(round(température,2))+' Celsius')
+        LCD.setTextLigne2("retour -> menu")
+        if LectBouton == "Back":
+            pageMenu = 0
 
 
 
