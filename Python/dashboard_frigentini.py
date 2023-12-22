eff6a3d4c0c76dbacea74b7c91e17c49ebb94051
mathias <mathias.puyfages@etu.umontpellier.fr>
2023-12-22 08:50:05 +0100

    Fichier temperature.csv et lecture de celui-ci pour le graphique journalier

--- Python/dashboard_frigentini.py
+++ Python/dashboard_frigentini.py
@@ -6,7 +6,8 @@ import plotly.express as px # interactive charts
 import matplotlib.pyplot as plt
 from PIL import Image
 
-#streamlit run dashboard_frigentini.py 
+#streamlit run dashboard_frigentini.py
+#!etre dans le repertoire Python
 
 image_directory = '../Image/icons8-frigo-50.png'
 image = Image.open(image_directory)
@@ -19,25 +20,22 @@ st.set_page_config(
 )
 
 @st.cache_data
-def get_data() -> pd.DataFrame:
-    return pd.read_csv('../CSV/frigo.csv')
+def get_data(chemin) -> pd.DataFrame:
+    return pd.read_csv(chemin)
 
-df = get_data()
+df = get_data('../CSV/frigo.csv')
 st.title("Frigentini Dashboard")
 st.markdown("## Temperature and storage information")
 
+df_temperature = get_data('../CSV/temperature.csv')
+
+pd_temperature = pd.DataFrame(df_temperature)
+
 # Prints the first lines of the filtered dataframe
-st.dataframe(df[0:10]) # prints only 5 lines
+st.dataframe(df) # prints only 5 lines
 
-temperature = pd.DataFrame([[5,0],[4,1],[3,2],[2,3],[1,4],[-1,5],[3.2,6],[5,7],[10,8],[30,9],[35,10],[5,11],[5,12],[5,13],[5,14],[5,15],[5,16],[5,17],[5,18]],columns = ["temperature","hour"])
 
-temperature2 = pd.DataFrame({
-    "temp" : [3,2,6,8] ,
-    "hour" : [0,9,10,14]}
+pd_temperature = pd_temperature.set_index("heure")
 
-)
-temperature2 = temperature2.rename(columns={'hour':'index'}).set_index('index')
+st.line_chart(pd_temperature, y = "temperature")
 
-st.line_chart(temperature, y = ["temperature"])
-
-st.line_chart(temperature2, y="temp")
