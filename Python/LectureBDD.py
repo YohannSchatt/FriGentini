1d11d478c05b159736e5877c8d9b31b7c49adae4
yohann.schatt <yohann.schatt@igtp3-pc09.isim.intra>
2023-12-22 11:23:15 +0100

    ajout des fonctions d'ajout dans la bdd

--- Python/LectureBDD.py
+++ Python/LectureBDD.py
@@ -7,9 +7,24 @@ def createdataframe(nom):
 Stock = createdataframe("../CSV/Stock.csv")
 Produits = createdataframe("../CSV/produits.csv")
 
-def ajoutelement(dataframe,tab):
-    
+def ajoutelement(data,elementajout):
+    if len(elementajout) == len(data.columns):
+        df2 = pd.DataFrame([elementajout], columns=data.columns)
+        data = data._append(df2, ignore_index=True)
+    return data
 
-print(Stock)
-print("")
-print(Produits)
+def datatocsv(data):
+    csvdata =  data.to_csv()
+    return csvdata
+
+print(len(Stock.columns))
+Stock = ajoutelement(Stock,[1,"pate","02/01/24","22/12/23"])
+
+print(datatocsv(Stock))
+
+def ecrireDansFichier(self,path,texte):
+        fichier = open(path,"w")
+        fichier.writelines(texte)
+        fichier.close()
+
+with open( )
