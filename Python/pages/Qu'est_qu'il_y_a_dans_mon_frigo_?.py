f95f409eafa513054076776cffd5c80de7ba3105
mathias <mathias.puyfages@etu.umontpellier.fr>
2023-12-23 21:50:30 +0100

    Ajout de la fonctionnalite de selection des jours pour la péremption et modification parametre page suivi temperature

--- Python/pages/Qu'est_qu'il_y_a_dans_mon_frigo_?.py
+++ Python/pages/Qu'est_qu'il_y_a_dans_mon_frigo_?.py
@@ -1,29 +1,46 @@
 import streamlit as st
 import Menu as mn
 import datetime
+from PIL import Image
 
+"""
+Fonction qui permet de savoir si une date du format jour/mois/année est dans moins du nombre de jour
+"""
 def est_date_courte (date:str,nb_jour:int) -> bool:
     auj = datetime.datetime.today()
     diff =  -(auj - datetime.datetime.strptime(date,'%d/%m/%Y')).days
-    return diff < nb_jour
+    return diff <= nb_jour
 
 
 
-on = st.toggle('Option date courte')
+def main() :
 
-if on:
-    nb_jour = st.select_slider(
-    "Selectionner le nombre de jour correspondant a une date courte",
-    options=range(10),
-    value=(3))
+    image_directory = '../Image/icons8-fromage-50.png'   
+    image = Image.open(image_directory)
 
-    df = mn.get_data('../CSV/frigo.csv')
+    #Bouton toggle
+    on = st.toggle('Option date courte')
 
-    st.write('Feature activated!')
-    filtered_df = df[df['date_péremption'].apply(est_date_courte, nb_jour=nb_jour)]
-    st.dataframe(filtered_df)
+    #condition si le bouton est activé
+    if on:
+        #Slider permettant de selectionner le nombre de jour a partir duquel on veut récupéré
+        nb_jour = st.select_slider(
+        "Selectionner le nombre de jour correspondant a une date courte",
+        options=range(10),
+        value=(3))
+        #Recuperation du CSV
+        df = mn.get_data('../CSV/frigo.csv')
 
-if not on :
-    st.write("Option désactivé")
-    df = mn.get_data('../CSV/frigo.csv')
-    st.dataframe(df) 
+        #On recupere seulement les lignes qui correspondent a nos jours de marge selectionné
+        filtered_df = df[df['date_péremption'].apply(est_date_courte, nb_jour=nb_jour)]
+        st.dataframe(filtered_df)
+
+    #Condition si le toggle est eteint
+    if not on :
+        #On récupère le CSV
+        df = mn.get_data('../CSV/frigo.csv')
+        st.dataframe(df) 
+
+
+if __name__ == '__main__':
+    main()
--- Python/pages/Suivi_temperature.py
+++ Python/pages/Suivi_temperature.py
@@ -1,13 +1,41 @@
 import pandas as pd
 import Menu as mn
 import streamlit as st
+from PIL import Image
 
 
-st.title("Suivi de la temperature")
-st.markdown("## Page qui représente le suivi des températures journalière ou hebdomadaire")
+def main () :
 
-df_temperature = mn.get_data('../CSV/temperature.csv')
+    image_directory = '../Image/icons8-thermomètre-50.png'   
+    image = Image.open(image_directory)
 
-pd_temperature = pd.DataFrame(df_temperature)
+    st.set_page_config(
+    page_title = 'Frigentini Temperature',  # important pour le référencement sur Google quand on hébergera l'app
+    page_icon = image,
+    layout = 'wide'
+    )
 
-st.line_chart(pd_temperature, y = "temperature")
+    st.title("Suivi de la temperature")
+    st.markdown("## Page qui représente le suivi des températures journalière ou hebdomadaire")
+
+    df_temperature = mn.get_data('../CSV/temperature.csv')
+
+    pd_temperature = pd.DataFrame(df_temperature)
+
+
+    option = st.selectbox(
+    "Quel détail pour le relevé ?",
+    ("Journalier", "Hebdomadaire"),
+    index=0,placeholder="Selectionner une fréquence ...",)
+
+    if option == "Journalier" : 
+        st.write("Affichage Journalier")
+    
+    if option == "Hebdomadaire" : 
+        st.write("affichage hebdomadaire")
+
+
+    st.line_chart(pd_temperature, y = "temperature")
+
+if __name__ == '__main__':
+    main()
