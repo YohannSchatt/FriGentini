06b877435f0c9b77833b9254706678cdb60ed59a
mathias <mathias.puyfages@etu.umontpellier.fr>
2023-12-30 10:35:33 +0100

    Ajout de la création des tables et insertion de données

--- Python/Connexion_DB.py
+++ Python/Connexion_DB.py
@@ -13,15 +13,53 @@ my_cursor = db.cursor()
 
 #my_cursor.execute("DROP DATABASE testdatabase") #Commande qui permet de supprimer une base de donnée
 
-my_cursor.execute("CREATE TABLE Produits (produitID int PRIMARY KEY AUTO_INCREMENT,name VARCHAR(50))")#Commande qui permet de creer une table dans la BD courante
+#my_cursor.execute("CREATE TABLE Produits (produitID int PRIMARY KEY AUTO_INCREMENT,name VARCHAR(50))")#Commande qui permet de creer une table dans la BD courante
 
-#my_cursor.execute("INSERT INTO Person (name, age) VALUES (%s,%s)",("Joe",22))
+#my_cursor.execute("DROP TABLE Produits") #
 
-#db.commit()
+#my_cursor.execute("INSERT INTO Person (name, age) VALUES (%s,%s)",("Joe",22)) #Bonne pratique 
+
+#db.commit() #Valider les changements dans la BD
+
 #my_cursor.execute("SELECT * FROM Person")
 
-#for x in my_cursor : 
+#for x in my_cursor : #Cursor est un élément itératif  
     #print(x)
+
+
+#########################################################################################################################################################################
+
+#!!! Faire gaffe supprime les valeurs enregistrées et declenche une erreur si les tables n'existent pas
+
+try : 
+    my_cursor.execute("DROP TABLE STOCK,TEMPERATURES,PRODUITS")
+
+except mysql.connector.Error as err: 
+    print(f"Erreur d'exécution de la commande SQL : {err}")
+
+# Lire les instructions SQL à partir du fichier
+with open('../SQL/creation_BD.sql', 'r') as file:
+    sql_commands = file.read().split('\n')
+
+# Exécuter chaque instruction SQL du fichier
+for command in sql_commands:
+    try:
+        my_cursor.execute(command)
+    except mysql.connector.Error as err:
+        print(f"Erreur d'exécution de la commande SQL : {err}")
+
+# Lire les instructions SQL à partir du fichier
+with open('../SQL/Donnees.sql', 'r') as file:
+    sql_commands = file.read().split('\n')
+
+# Exécuter chaque instruction SQL du fichier
+for command in sql_commands:
+    try:
+        my_cursor.execute(command)
+    except mysql.connector.Error as err:
+        print(f"Erreur d'exécution de la commande SQL : {err}")
+
+db.commit() #Valider les changements dans la BD
 
 my_cursor.close()
 db.close()
