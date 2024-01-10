68eba4f0c1f42628d6eca538193b8d7a90ad9416
yohann.schatt <yohann.schatt@igtp3-pc12.isim.intra>
2024-01-10 09:59:56 +0100

    ajout des paramètres et de l'alarme

--- Python/main.py
+++ Python/main.py
@@ -10,11 +10,14 @@ import time
 import DriverThermometre as thermo
 import grovepi 
 import pandas as p
+import led 
 
 buttonOk = 4
 buttonBack = 7
 buttonPlus = 2
 buttonMoins = 3
+buzzer = 6
+diode = 5 
 
 LCD.initialisation()
 LCD.effacerText()
@@ -47,11 +50,29 @@ def LectBouton():
     if grovepi.digitalRead(buttonMoins) == 1:
         return "Moins"
 
-pageMenu = 0
-selectionPage = 1
+def Alarme(temperatureAct,temperature,approximation,Alarme):
+    if Alarme and (temperatureAct < temperature - approximation or temperatureAct > temperature + approximation):
+        led.turnON(buzzer)
+        led.turnON(diode)
+    else :
+        led.turnOFF(buzzer)
+        led.turnOFF(buzzer)
+
+temp = [6,1] #[température défini, approximation défini]
+temperature = 6 #température initial pris par le système
+approximation = 1 #approximation initial pris par le système 
+AlarmeActive
+
+pageMenu = 0 #Int qui permet de changer de Menu
+selectionPage = 1 #Int qui permet de défiler entre les différents page du menu
+pageParamètre = 0 #Int qui permet savoir ou on est dans les paramètres
+poscursor= 0 #Int qui permet de connaitre ou se situe le curseur dans paramètre
+cursor = ["<-",""] # curseur utilisé dans les différents menu qui se déplace sur les deux lignes
+Alarme = True #variable pour savoir si l'alarme est active ou non
 while True:
+    températureAct = thermo.ReadTemperature()
     Bouton = LectBouton()
-    if pageMenu == 0:
+    if pageMenu == 0: #Menu de selection
         LCD.setTextLigne1("    Selection")
         if selectionPage == 1:
             LCD.setTextLigne2("< affiche Temp >")
@@ -69,10 +90,14 @@ while True:
             LCD.setTextLigne2("<  suppr data  >")
             if Bouton == "Ok":
                 pageMenu = 4
-        if selectionPage == 5:
-            LCD.setTextLigne2("<   Eteindre   >")
+        if selectionPage == 5: #Paramètres
+            LCD.setTextLigne2("<  Parametres  >")
             if Bouton == "Ok":
                 pageMenu = 5
+        if selectionPage == 6: #Stoppe le programme
+            LCD.setTextLigne2("<   Eteindre   >")
+            if Bouton == "Ok":
+                pageMenu = 6
         if Bouton == "Plus":
             selectionPage = (selectionPage+1)%5
         if Bouton == "Moins":
@@ -81,11 +106,35 @@ while True:
             else :
                 selectionPage = selectionPage - 1
         time.sleep(0.12)
-    if pageMenu == 1 :
-        LCD.setTextLigne1(str(round(thermo.ReadTemperature(),2))+' Celsius       ')
+    if pageMenu == 1 : #Affiche la température
+        LCD.setTextLigne1(str(round(températureAct))+' Celsius       ')
         LCD.setTextLigne2("retour -> menu ")
-        if Bouton == "Back":
+        if Bouton == "Back": #permet de faire retour
             pageMenu = 0
+    if pageMenu == 6 : #Paramètre
+        if pageParamètre = 0 : # Menu principale des paramètres
+            LCD.setTextLigne1("temp : " + str(temp[0]) + " ± "+ str(temp[1]) + " " + cursor[poscursor] + "       ")
+            LCD.setTextLigne2("Alarme : " + str(Alarme) +  cursor[(poscursor+1)%2] + "       ")
+            if Bouton == "Moins" or Bouton == "Plus": # Permet de déplacer le curseur
+                if SelectionParamètre == 0:
+                    SelectionParamètre = 1
+                else :
+                    SelectionParamètre = 0
+        else if pageParamètre = 1 :  #Menu selection
+            LCD.setTextLigne1("temp : " + str(temp[0]) + cursor[(poscursor+1)%2] + "         ")
+            LCD.setTextLigne2("approx : " + str(temp[1]) + cursor[(poscursor+1)%2] + "       ") 
+            if Bouton == "Plus":
+                temp[poscursor] +=0.1
+            if Bouton == "Moins":
+                temp[poscursor] -=0.1
+
+            if Bouton == "Back" or Bouton == "Ok": #permet de faire retour
+                pageMenu = 0
+        else if Bouton == "Back": #permet de faire retour
+            pageMenu = 0
+
+        
+
 
 
 
