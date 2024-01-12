1082a0943a2bca319c0dc3fc67fbbd15a80fd51b
yohann.schatt <yohann.schatt@igtp3-pc12.isim.intra>
2024-01-12 18:37:16 +0100

    correction de bug

--- Python/main.py
+++ Python/main.py
@@ -18,10 +18,6 @@ event_Bouton = threading.Event()
 event_Menu = threading.Event()
 
 def LectBouton():
-    buttonOk = 4
-    buttonBack = 7
-    buttonPlus = 2
-    buttonMoins = 3
     print("je suis dans bouton")
     if grovepi.digitalRead(buttonOk) == 1:
         Bouton = "Ok"
@@ -150,6 +146,10 @@ def pageMenu5(Bouton):
 
 
 def main():
+    buttonOk = 4
+    buttonBack = 7
+    buttonPlus = 2
+    buttonMoins = 3
     buzzer = 6
     diode = 8
 
