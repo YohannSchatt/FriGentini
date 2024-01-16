db08c213a09e681b3f409758a065b8e4210f44be
mathias <mathias.puyfages@etu.umontpellier.fr>
2024-01-16 17:31:19 +0100

    Ajout print

--- Python/main.py
+++ Python/main.py
@@ -203,6 +203,7 @@ def pageMenu2():
             df_frigo.loc[len(df_frigo.index)] = [len(df_frigo)+1,'5dc2f869',menu.date_peremption.strftime('%d/%m/%Y'),menu.date.strftime('%d/%m/%Y')] #Ajout d'une ligne dans le csv de la liste des produits dans le stock
             LCD.effacerText()
             LCD.setTextLigne1("Produit ajouté")
+            print(df_frigo)
             time.sleep(1)
         else : 
             print("mauvaise commande")
