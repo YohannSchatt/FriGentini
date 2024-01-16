3f68bcef45365afc9c24dd88482f1ad457580b6d
mathias <mathias.puyfages@etu.umontpellier.fr>
2024-01-16 17:37:04 +0100

    Ajout Modif coquille

--- Python/main.py
+++ Python/main.py
@@ -191,7 +191,7 @@ def pageMenu2():
         print("Je rentre dans pageAjout 1 et le bouton vaut ", menu.Bouton)
         print("La date sélectionné est : " + str(menu.date_peremption))
         LCD.setTextLigne1("Date peremption")
-        LCD.setTextLigne2(str(menu.date_peremption[0:8]))
+        LCD.setTextLigne2(str(menu.date_peremption)[0:8])
 
         if menu.Bouton == "Plus" :
             menu.date_peremption = menu.date_peremption + menu.delta
