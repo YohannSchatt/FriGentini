c1f143b04b1cdfc063874d0d4fc7704f46b95c99
mathias <mathias.puyfages@etu.umontpellier.fr>
2024-01-22 18:50:16 +0100

    Ajout du contenu du mail

--- Python/Mail_Test.py
+++ Python/Mail_Test.py
@@ -2,6 +2,23 @@
 import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import pandas as pd
+import datetime
+
+def get_data(chemin) -> pd.DataFrame:
+    return pd.read_csv(chemin)
+
+def est_périmé (date:str) -> bool : 
+    auj = datetime.datetime.today()
+    diff =  -(auj - datetime.datetime.strptime(date,'%d/%m/%Y')).days
+    return diff < 0
+
+
+def est_date_courte (date:str,nb_jour:int) -> bool:
+    auj = datetime.datetime.today()
+    diff =  -(auj - datetime.datetime.strptime(date,'%d/%m/%Y')).days
+    print(diff)
+    return  0 <= diff <= nb_jour
 
 # on rentre les renseignements pris sur le site du fournisseur
 smtp_address = 'smtp.gmail.com'
@@ -12,7 +29,7 @@ email_address = 'frigentini@gmail.com'
 email_password = 'bzkj puyn afbc rghm '
 
 # on rentre les informations sur le destinataire
-email_receiver = 'mathnor01@gmail.com'
+email_receiver = 'yohann.schatt@gmail.com'
 
 
 # on crée un e-mail
@@ -24,6 +41,36 @@ message["From"] = email_address
 # un destinataire
 message["To"] = email_receiver
 
+df = get_data('../CSV/frigo.csv')
+
+#On recupere seulement les lignes qui correspondent a nos jours de marge selectionné
+filtered_df = df[df['date_péremption'].apply(est_date_courte, nb_jour=3)]
+df1 = get_data('../CSV/liste_produits.csv')
+df2 = pd.merge(filtered_df,df1,left_on= 'Type_Produit',right_on='Code_barre')#Liste des produits proche de la date < 3 jours
+
+print(df2)
+
+filtered_df = df[df['date_péremption'].apply(est_périmé)]
+df3 = pd.merge(filtered_df,df1,left_on= 'Type_Produit',right_on='Code_barre')#Liste des produits périmés 
+
+print(df3)
+
+def formattage (df : pd.DataFrame) -> str: 
+    sortie = ""
+    for i in df.index:
+        ligne = df[["nom","date_péremption"]].iloc[i].values[0:2]
+        nom = ligne[0]
+        date = ligne[1]
+        sortie += "<tr>\n"
+        sortie += "<td>" + str(nom) + "</td>\n"
+        sortie += "<td>" + str(date) + "</td>\n"
+        sortie += "</tr>\n"
+    return sortie
+        
+        
+
+print(formattage(df2))
+
 # sa version HTML
 html = '''
 <html>
@@ -34,15 +81,7 @@ html = '''
     <tr>
         <th>Produit</th>
         <th>Date de Péremption</th>
-    </tr>
-    <tr>
-        <td>Pain</td>
-        <td>2024-01-31</td>
-    </tr>
-    <tr>
-        <td>Lait</td>
-        <td>2024-02-15</td>
-    </tr>
+    </tr>'''+ formattage(df3)+ '''
 </table>
 
 
@@ -51,15 +90,7 @@ html = '''
     <tr>
         <th>Produit</th>
         <th>Date de Péremption</th>
-    </tr>
-    <tr>
-        <td>Pain</td>
-        <td>2024-01-31</td>
-    </tr>
-    <tr>
-        <td>Lait</td>
-        <td>2024-02-15</td>
-    </tr>
+    </tr>'''+formattage(df2) +'''
 </table>
 <b>Cdt</b> <br>
 </body>
